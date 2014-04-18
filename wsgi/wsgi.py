c6f29eee1352d344d63eddce1f5d77a921a720a9
Jin Sun <callmesunjplz@gmail.com>
2014-04-17 22:58:55 -0500

    change wsgi

--- wsgi/wsgi.py
+++ wsgi/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Sourcefinder.settings.configuration")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.configuration")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
